6829d72a911e27d81e122414de46efd414b7431c
MrTinker64 <avi.micah@gmail.com>
2024-12-23 18:29:25 -0500

    finish tuning module sim drive kp and kd

--- src/main/java/frc/robot/subsystems/drive/DriveConstants.java
+++ src/main/java/frc/robot/subsystems/drive/DriveConstants.java
@@ -109,8 +109,8 @@ public final class DriveConstants {
         break;
       case SIM:
         driveFeedforward = new SimpleMotorFeedforward(0.0, 0.13);
-        drivekP = new LoggedTunableNumber("Drive/ModuleTunables/drivekP", 0.28);
-        drivekD = new LoggedTunableNumber("Drive/ModuleTunables/drivekD", 0.0);
+        drivekP = new LoggedTunableNumber("Drive/ModuleTunables/drivekP", 0.25); // tuned
+        drivekD = new LoggedTunableNumber("Drive/ModuleTunables/drivekD", 0.0); // tuned
         turnkP = new LoggedTunableNumber("Drive/ModuleTunables/turnkP", 10);
         turnkD = new LoggedTunableNumber("Drive/ModuleTunables/turnkD", 0.0);
         break;
