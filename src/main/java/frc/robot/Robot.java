54252c8d2870f57b5910fa8f2b32970f39fae2e0
MrTinker64 <avi.micah@gmail.com>
2024-12-28 15:24:38 -0500

    move build constants to the constants folder too

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -19,6 +19,7 @@ import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.constants.BuildConstants;
 import frc.robot.constants.Constants;
 import frc.robot.subsystems.leds.Leds;
 import frc.robot.util.Alert;
